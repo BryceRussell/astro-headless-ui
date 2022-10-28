165a7daa0af31f914b8320f5bccb4d8d161b76c8
BryceRussell <19967622+BryceRussell@users.noreply.github.com>
2022-10-27 20:09:56 -0500

    Add JS Enhanced CSS Properties Components
    - Scroll X/Y position properties component
    - Mouse X/Y position properties component
    - NoScript property for disabled javascript component

---
+++ package/css/index.js
@@ -0,0 +1,10 @@
+// CSS properties
+import ScrollProp from './ScrollProp.astro';
+import MouseProp from './MouseProp.astro';
+import NoScriptProp from './NoScriptProp.astro';
+
+export {
+    ScrollProp,
+    MouseProp,
+    NoScriptProp,
+};
--- package/index.js
+++ package/index.js
@@ -1,10 +1,21 @@
+// CSS
+import ScrollProp from './css/ScrollProp.astro';
+import MouseProp from './css/MouseProp.astro';
+import NoScriptProp from './css/NoScriptProp.astro';
+// Static
 import Paginate from './pagination/Paginate.astro';
 import Pagination from './pagination/Pagination.astro';
 import Breadcrumb from './breadcrumb/Breadcrumb.astro';
 import A from './a/A.astro';
 import Map from './map/Map.astro';
+// Client
+
+
 
 export {
+    ScrollProp,
+    MouseProp,
+    NoScriptProp,
     Paginate,
     Pagination,
     Breadcrumb,
