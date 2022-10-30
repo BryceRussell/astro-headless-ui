d5dc67021cb6d3f290b80584e6e7ebe0d74414f4
BryceRussell <19967622+BryceRussell@users.noreply.github.com>
2022-10-30 00:41:22 -0500

    Add Icon, wrapper for iconify-icon web component

--- package/index.js
+++ package/index.js
@@ -1,24 +1,41 @@
-// CSS
+// CSS Components
+// HTML/CSS
+import NoScriptProp from './css/NoScriptProp.astro';
+// Client Scripts
 import ScrollProp from './css/ScrollProp.astro';
 import MouseProp from './css/MouseProp.astro';
-import NoScriptProp from './css/NoScriptProp.astro';
-// Static
+import KeyboardProp from './css/KeyboardProp.astro';
+import NetworkProp from './css/NetworkProp.astro';
+
+
+
+// SSG/SSR Components
 import Paginate from './pagination/Paginate.astro';
 import Pagination from './pagination/Pagination.astro';
 import Breadcrumb from './breadcrumb/Breadcrumb.astro';
 import A from './a/A.astro';
+import Rating from './rating/Rating.astro';
+import Icon from './icon/Icon.astro'
 import Map from './map/Map.astro';
-// Client
+
+
+
+// Client Side Components
 
 
 
 export {
     ScrollProp,
     MouseProp,
+    KeyboardProp,
     NoScriptProp,
+    NetworkProp,
+
     Paginate,
     Pagination,
     Breadcrumb,
     A,
+    Rating,
+    Icon,
     Map
 };
