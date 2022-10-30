f18597b220e120b97742d50451f58b55e13e2320
BryceRussell <19967622+BryceRussell@users.noreply.github.com>
2022-10-30 00:39:50 -0500

    Add KeyboardProp and NetworkProp, Tweak other CSS Prop Scripts
    - Add KeyboardProp Last key pressed inside a css variable
    - Add NetworkProp Network Information API inside css variables
    - Added ability to change the element variables are attached to in all scripts, default to 'selector',  same element event listeners use

--- package/css/index.js
+++ package/css/index.js
@@ -1,10 +1,14 @@
 // CSS properties
 import ScrollProp from './ScrollProp.astro';
 import MouseProp from './MouseProp.astro';
+import KeyboarProp from './KeyboarProp.astro';
 import NoScriptProp from './NoScriptProp.astro';
+import NetworkProp from './NetworkProp.astro';
 
 export {
     ScrollProp,
     MouseProp,
+    KeyboarProp,
     NoScriptProp,
+    NetworkProp
 };
